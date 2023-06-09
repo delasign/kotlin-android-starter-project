aaf0007d8ecac6dc176b09be33f9f99028cda022
Oscar de la Hera Gomez <oscar@delasign.com>
2023-06-09 17:29:57 -0400

    How to add a custom font to an Android Studio project

--- app/src/main/java/com/delasign/samplestarterproject/ui/theme/Type.kt
+++ app/src/main/java/com/delasign/samplestarterproject/ui/theme/Type.kt
@@ -2,9 +2,33 @@ package com.delasign.samplestarterproject.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.delasign.samplestarterproject.R
+
+// CREATE THE FONT FAMILY
+val Roboto = FontFamily(
+    Font(R.font.roboto_black, FontWeight.Black, FontStyle.Normal),
+    Font(R.font.roboto_black_italic, FontWeight.Black, FontStyle.Italic),
+
+    Font(R.font.roboto_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.roboto_italic, FontWeight.Bold, FontStyle.Italic),
+
+    Font(R.font.roboto_medium, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.roboto_medium_italic, FontWeight.Medium, FontStyle.Italic),
+
+    Font(R.font.roboto_regular, FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.roboto_italic, FontWeight.Normal, FontStyle.Italic),
+
+    Font(R.font.roboto_light, FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.roboto_light_italic, FontWeight.Light, FontStyle.Italic),
+
+    Font(R.font.roboto_thin, FontWeight.Thin, FontStyle.Normal),
+    Font(R.font.roboto_thin_italic, FontWeight.Thin, FontStyle.Italic),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
