07183c688c8d2ac437a72c58d7b2b39262f4b8bd
Oscar de la Hera Gomez <oscar@delasign.com>
2023-06-12 12:51:16 -0400

    How to add and use custom colors in an Android project

--- app/src/main/java/com/delasign/samplestarterproject/ui/theme/Color.kt
+++ app/src/main/java/com/delasign/samplestarterproject/ui/theme/Color.kt
@@ -2,6 +2,8 @@ package com.delasign.samplestarterproject.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val CustomGrey = Color(0xFF949494)
+
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
