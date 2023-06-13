186123c482696f738ce8fc6705877c40fab9a74c
Oscar de la Hera Gomez <oscar@delasign.com>
2023-06-13 13:40:23 -0400

    Added Constants and Debugging Identifiers

---
+++ app/src/main/java/com/delasign/samplestarterproject/models/constants/Constants.kt
@@ -0,0 +1,9 @@
+package com.delasign.samplestarterproject.models.constants
+
+import androidx.compose.ui.unit.dp
+
+// MARK: Visual Constants
+val kButtonDimension = 60.dp
+val kPadding = 20.dp
+val kShadowSize = 4.dp
+const val kAnimationConstant = 0.5
---
+++ app/src/main/java/com/delasign/samplestarterproject/models/constants/DebuggingIdentifiers.kt
@@ -0,0 +1,9 @@
+package com.delasign.samplestarterproject.models.constants
+
+object DebuggingIdentifiers {
+    val actionOrEventSucceded: String = "✅"
+    val actionOrEventInProgress: String = "⚈ ⚈ ⚈"
+    val actionOrEventFailed: String = "❌"
+    val notificationSent: String = "📤"
+    val notificationRecieved: String = "📥"
+}
